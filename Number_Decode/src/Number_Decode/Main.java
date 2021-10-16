fa474f083f9311adaebe89ba2527dac74fd11df3
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-10-15 23:44:25 -0400

    Update Main.java

--- Number_Decode/src/Number_Decode/Main.java
+++ Number_Decode/src/Number_Decode/Main.java
@@ -2,6 +2,11 @@ package Number_Decode;
 
 public class Main {
     public static void main (String[] args){
-        System.out.println();
+        Number_Decode ND = new Number_Decode();
+        String userInput = "226";
+        int length = userInput.length();
+        int result;
+        result = ND.number_decode(userInput,length);
+        System.out.println(result);
     }
 }
