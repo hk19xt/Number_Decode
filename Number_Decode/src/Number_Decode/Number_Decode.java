793e6df41cebcc94f01a3d2f4c1a453e1c6c2eb7
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-10-15 23:44:39 -0400

    Update Number_Decode.java

--- Number_Decode/src/Number_Decode/Number_Decode.java
+++ Number_Decode/src/Number_Decode/Number_Decode.java
@@ -1,4 +1,24 @@
 package Number_Decode;
 
 public class Number_Decode {
+    public int helper(String userInput, int length){
+        if(length ==0){
+            return 1;
+        }
+        int start = userInput.length() - length;
+        if(userInput.charAt(start)=='0'){
+            return 0;
+        }
+
+        int result = helper(userInput, length-1);
+        if(length>=2 && Integer.parseInt(userInput.substring(start,start+2))<=26){
+            result = result + helper(userInput, length-2);
+        }
+        return result;
+
+    }
+
+    public int number_decode(String userInput, int length){
+        return helper(userInput, length);
+    }
 }
