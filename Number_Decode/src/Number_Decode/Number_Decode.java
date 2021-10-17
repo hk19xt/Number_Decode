7163ecfbf4004d1a458f5126b2dbd7e6cbe5e124
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-10-16 22:19:38 -0400

    Update Number_Decode.java

--- Number_Decode/src/Number_Decode/Number_Decode.java
+++ Number_Decode/src/Number_Decode/Number_Decode.java
@@ -1,24 +1,37 @@
 package Number_Decode;
 
+/*This class is the implementation class. 
+The possible number of decoding the user input string will be calculated recursively. The detailed explanation will be provided below. */
+
 public class Number_Decode {
+
+    
+    //This helper will calculate whether each character can be decoded or not, adding the number of possible ways to decode.
     public int helper(String userInput, int length){
-        if(length ==0){
+         
+        int start = userInput.length() - length;
+        
+        //Base case : if there is no string by user input, then it will return one because there is only one way to decode it.
+        if(length == 0){
             return 1;
         }
-        int start = userInput.length() - length;
-        if(userInput.charAt(start)=='0'){
+        
+        //Before starting the calculation, we should consider the base case conditions.
+        if(userInput.charAt(start)=='0'){ //If there is zero at the beginning of the string, then it will return zero.
             return 0;
         }
 
         int result = helper(userInput, length-1);
-        if(length>=2 && Integer.parseInt(userInput.substring(start,start+2))<=26){
+        
+        if(Integer.parseInt(userInput.substring(start,start+2))<=26 && length>=2){
             result = result + helper(userInput, length-2);
         }
         return result;
 
     }
 
+    //This method is for calculating the possible number of decoding way recursively.
     public int number_decode(String userInput, int length){
         return helper(userInput, length);
     }
-}
+}//Number_Decode
