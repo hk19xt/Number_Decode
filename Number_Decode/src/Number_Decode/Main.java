fe73e1aa86598ee3d1401a9722aa7f9e87e53166
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-10-16 22:11:30 -0400

    Update Main.java

--- Number_Decode/src/Number_Decode/Main.java
+++ Number_Decode/src/Number_Decode/Main.java
@@ -1,12 +1,20 @@
 package Number_Decode;
 
+/* This class is the main class. 
+The users can change their input string values in the hardcoded line. (The length will be automatically calculated.) 
+The output will be calculated recursively in the implementation class.
+
+Name: Hyejin Kim
+Date: 10/16/2021
+*/
+
 public class Main {
     public static void main (String[] args){
         Number_Decode ND = new Number_Decode();
-        String userInput = "226";
-        int length = userInput.length();
+        String userInput = "3241"; //Users can change their input string value here.
+        int length = userInput.length(); //The length of a user's input string.
         int result;
         result = ND.number_decode(userInput,length);
         System.out.println(result);
     }
-}
+}//Main
